0196cbc680e22dd885101eb40a2f074909048dff
Artem1899 <studentusla18@gmail.com>
2023-04-09 15:02:05 +0500

    Изменил некоторые комментарии в файле и добавил новые

--- less1.6.4.py
+++ less1.6.4.py
@@ -1,6 +1,6 @@
+# задание из https://stepik.org/lesson/138920/step/4?unit=196194
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import math
 import time
 
 link = "http://suninjuly.github.io/simple_form_find_task.html"
@@ -21,10 +21,8 @@ try:
     button.click()
 
 finally:
-    # успеваем скопировать код за 30 секунд
+    # таймер чтобы успеть скопировать ответ из alert
     time.sleep(20)
-    # закрываем браузер после всех манипуляций
+    # закрытие браузера после теста
     browser.quit()
-
-# не забываем оставить пустую строку в конце файла
 
